9eecea14c5e5ac321c8b150acd1525e051efa5b7
Marvin Frick <marvin.frick@shopify.com>
2019-12-28 04:08:00 +0100

    initial commit 2.0
    Now with a single output, not one per class.

--- convert.rb
+++ convert.rb
@@ -1,20 +1,6 @@
 require 'json'
-CLASS_NAMES_TO_ABBRV = {
-  "Warlock" => 'L',
-  "Warrior" => "W",
-  "Mage" => "M",
-  "Paladin" => "Pa",
-  "Priest" => "Pr",
-  "Rogue" => "R",
-  "Shaman" => "S",
-  "Druid" => "D",
-  "Hunter" => "H"
-}
-
 class Task
-  def initialize(class_name)
-    @class_filter_name = class_name
-
+  def initialize()
     @index = nil
     @action = nil
     @race_specific = nil
@@ -36,17 +22,13 @@ class Task
 
   def parse_to_self!(list_tingy)
     @index, @action, @race_specific, @class_specific, @class_specific_2, _, @subject_id, _,  @subject_name, @target_name, @target_id, _, @coords, @zone, _, @requires, @comment = list_tingy
-    if @class_specific.include?(@class_filter_name) or @class_specific.empty?
-      self
-    else
-      nil
-    end
+    self
   end
 
   def class_restriction
     class_names = {
       "L" => "Warlock",
-      "w" => "Warrior",
+      "W" => "Warrior",
       "M" => "Mage",
       "Pa" => "Paladin",
       "Pr" => "Priest",
@@ -62,12 +44,20 @@ class Task
       class_names.each do |class_abbrv, class_name|
         applicable_classes << class_name if @class_specific.include?(class_abbrv)
       end
-      " (#{applicable_classes.join(', ')} only)"
+      " [A #{applicable_classes.join(', ')}](#{applicable_classes.join(', ')} only)"
     end
   end
+
   def race_restriction
     race_names = {
-    # TODO
+      "O" => "Orc",
+      "T" => "Troll",
+      "U" => "Undead",
+      "C" => "Tauren",
+      "H" => "Human",
+      "D" => "Dwarf",
+      "G" => "Gnome",
+      "N" => "Nightelf"
     }
     if @race_specific.empty?
       ""
@@ -76,26 +66,95 @@ class Task
       race_names.each do |race_abbrv, race_name|
         applicable_races << race_name if @race_specific.include?(race_abbrv)
       end
-      " (#{applicable_races.join(', ')} only)"
+      " [A #{applicable_races.join(', ')}](#{applicable_races.join(', ')} only)"
     end
+  end
+
+  def second_line_comment
+    @comment.empty? ? "" : "\\\\*#{@comment.gsub('[','<').gsub(']','>')}*"
+  end
+
+  def location
+    "#{coords},#{zone}"
+  end
+
+  def zone
+    @zone.split("-").map(&:capitalize).join(" ")
+  end
+
+  def subject_id
+    @subject_id.empty? ? "" : " #{@subject_id.gsub('[','').gsub(']','')} "
+  end
+
+  def subject_name
+    @subject_name.empty? ? "" : " #{@subject_name.gsub('[','<').gsub(']','>')} "
+  end
+
+  def target_name
+    "#{@target_name}[G#{location}]"
+  end
+
+  def coords
+    "#{@coords.gsub('~','')}"
+  end
+
+  def qa
+    return "" if subject_id.empty?
+    "[QA#{subject_id} #{subject_name}]"
+  end
+
+  def qs
+    return "" if subject_id.empty?
+    "[QS#{subject_id} #{subject_name}]"
+  end
+
+  def qt
+    return "" if subject_id.empty?
+    "[QT#{subject_id} #{subject_name}]"
+  end
+
+  def qc
+    return "" if subject_id.empty?
+    "[QC#{subject_id} #{subject_name}]"
+  end
+
+  def xp
+    min, max, level = @subject_name.match(/to (\d+) \/ (\d+) L(\d+)/)&.captures
+    "[XP#{level}+#{min}]"
   end
 
   def to_s
     task_string = case @action
+    when /DING/
+      "DING [XP#{@action.gsub('DING','').strip}]"
     when /Pick Up/
-      "#{@action} [QA#{@subject_id} #{@subject_name}] from #{@target_name}"
+      "#{@action}#{qa} from #{target_name}"
+    when /Skip/
+      "#{@action}#{qs} from #{target_name}"
     when /Hand In/
-      "#{@action} [QT#{@subject_id} #{@subject_name}] at #{@target_name}"
+      "#{@action}#{qt} at #{target_name}"
     when /Complete Quest/
-      "#{@action} [QC#{@subject_id} #{@subject_name}]"
+      "#{@action}#{qc}"
     when /Set Hearth/
-      "#{@action} [S] #{@subject_name.gsub('[','<').gsub(']','>')}"
+      "[S] #{@action}#{subject_name}"
     when /Vendor/
-      "#{@action} at #{@target_name.gsub('[','<').gsub(']','>')}"
+      "#{@action} at [V]#{target_name}"
+    when /Buy/
+      "#{@action}#{subject_name}at [V]#{target_name}"
+    when /Train/
+      "[T] #{@action} #{subject_name}at #{target_name}"
+    when /Fly/
+      "Fly to [F #{subject_name.gsub('to','').gsub('The','').strip}]"
+    when /Get Flight Path/
+      "[P] #{@action}#{subject_name}at #{target_name}"
+    when /Go/
+      "[G #{@coords},#{zone}] #{@action}#{subject_name}"
+    when /Grind/
+      "Gring until #{xp}"
     else
-      "#{@action} #{@subject_name.gsub('[','<').gsub(']','>')}"
+      "#{@action} #{subject_name}"
     end
-    "#{task_string}#{class_restriction}#{race_restriction}"
+    "#{task_string}#{class_restriction}#{race_restriction}#{second_line_comment}"
   end
 end
 class Step
@@ -112,9 +171,8 @@ end
 class Chapter
   attr_accessor :steps, :guide_name
   attr_reader :index
-  def initialize(index, faction_name, class_name)
+  def initialize(index, faction_name)
     @faction_filter_name = faction_name
-    @class_filter_name = class_name
     @index = index
     @steps = []
     @guide_name = nil
@@ -133,7 +191,7 @@ class Chapter
     "Guidelime.registerGuide([[\n"+
     "[N #{name}]\n"+
     "[NX #{next_chapter_name}]\n"+
-    "[GA #{@faction_filter_name},#{@class_filter_name}]\n"+
+    "[GA #{@faction_filter_name}]\n"+
     "[D ClassicWoW.live Leveling Guide for #{@name} converted to Guidelime by Marv]\n"+
     @steps.map(&:to_s).join("\n") +
     "\n]], \"ClassicWoW.live\")"
@@ -141,16 +199,15 @@ class Chapter
 end
 class Guide
   attr_accessor :chapters
-  def initialize(name, class_name)
+  def initialize(name)
     @name = name
-    @class_name = class_name
     @chapters = []
   end
 
   def to_guidelime!
     chapters.each do |chapter|
-      chapter.guide_name = @name+"_"+@class_name
-      File.open("Guidelime_ClassicWoWdotLive_#{@class_name}/"+chapter.name+ ".lua", 'w') do |f|
+      chapter.guide_name = @name
+      File.open(chapter.name+ ".lua", 'w') do |f|
         f.write(chapter.to_s)
       end
     end
@@ -165,17 +222,16 @@ if ARGV[0]
   when /Horde/
     "Horde"
   end
-  class_name = ARGV[1].chomp
   leveling = JSON.parse(File.read(ARGV[0]))
   puts "Read #{name}"
-  @guide = Guide.new(name, class_name)
+  @guide = Guide.new(name)
   leveling.each do |group_index, steps|
-    sg = Chapter.new(group_index, faction_name, class_name)
+    sg = Chapter.new(group_index, faction_name)
     steps.each do |step_index, tasks|
        step = Step.new(step)
        tasks.each do |task_as_list_thingy|
-        task = Task.new(CLASS_NAMES_TO_ABBRV[class_name])
-        step.tasks << task if task.parse_to_self!(task_as_list_thingy)
+        task = Task.new
+        step.tasks << task.parse_to_self!(task_as_list_thingy)
        end
        sg.steps << step
     end
--- convert_all.sh
+++ convert_all.sh
@@ -1,9 +1,5 @@
 rm -f **/*.lua
 rm -f **/*.toc
-for  class in Warrior Warlock Mage Paladin Priest Rogue Shaman Druid Hunter; do
-  for f in *.json; do ruby convert.rb "${f}" "${class}"; done
-  cd Guidelime_ClassicWoWdotLive_${class}
-  cp Guidelime_ClassicWoWdotLive_${class}.toc.template Guidelime_ClassicWoWdotLive_${class}.toc
-  for lua in *.lua; do echo ${lua} >> Guidelime_ClassicWoWdotLive_${class}.toc; done
-  cd -
-done
+for f in *.json; do ruby convert.rb "${f}"; done
+cp Guidelime_ClassicWoWdotLive.toc.template Guidelime_ClassicWoWdotLive.toc
+for lua in *.lua; do echo ${lua} >> Guidelime_ClassicWoWdotLive.toc; done
